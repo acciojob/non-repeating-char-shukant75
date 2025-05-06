5f326cc07e9cdc575a7fdd5d0700eef2e3ed1ef8
shukant75 <spanjikar9155@gmail.com>
2025-05-07 05:07:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	function firstNonRepeatedChar(str) {
+  const charCount = {};
+
+  // First pass: Count each character
+  for (let char of str) {
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+
+  // Second pass: Find the first non-repeated character
+  for (let char of str) {
+    if (charCount[char] === 1) {
+      return char;
+    }
+  }
+
+  return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+
+}
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
